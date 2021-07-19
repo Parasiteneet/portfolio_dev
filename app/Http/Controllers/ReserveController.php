20c351a8b47235f147e0c00b9ceeb3ead2034e7f
rollo <wumabeatboxer@yahoo.co.jp>
2021-07-20 01:17:26 +0900

    Validation

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -6,22 +6,35 @@ use Illuminate\Http\Request;
 
 class ReserveController extends Controller
 {
+       /**
+     * 予約画面の表示
+     *
+     * 
+     */
     public function index() 
     {
-        return view('/book/reserve');
+        return view('/book.reserve');
     }
     
+     /**
+     * 予約画面で入力された画面を確認
+     *
+     * @param  Request  $request
+     */
     public function confirm(Request $request) 
     {
-        // $request->validate([
-        //     'booking-name' => 'required',
-        //     'booking-tel' => 'required|unique',
-        //     'booking-day' => 'required',
-        //     'scheduled-time' => 'required',
-        //     'form-comment' => 'max:100',
-        // ]);
 
-        return view('/book/confirm');
+        $request->validate([
+            'booking-name' => 'required',
+            'booking-tel' => 'required|unique',
+            'booking-day' => 'required',
+            'scheduled-time' => 'required',
+            'form-comment' => 'max:100',
+        ]);
+
+        $inputs = $request->all();
+
+        return view('/book.confirm', ['inputs' => $inputs,]);
 
     }
 
--- resources/views/book/reserve.blade.php
+++ resources/views/book/reserve.blade.php
@@ -3,27 +3,27 @@
 @section('content')
 
   <div class="reservation-form">
-    <form method="POST" action="/book/confirm">
+    <form method="POST" action="book/confirm">
     @csrf
       <div class="form-groupe">
         <label for="booking-name">お名前</label>
-          <input type="text" class="form-control" id="booking-name" aria-describedby="input_name" placeholder="お名前を入力して下さい">
+          <input type="text" value="{{ old('booking-name') }}" class="form-control" id="booking-name" aria-describedby="input_name" placeholder="お名前を入力して下さい">
       </div>
       <div class="form-groupe mt-3">
         <label for="booking-tel">お電話番号</label>
-          <input type="tel" class="form-control" id="booking-tel" aria-describedby="input_tel" placeholder="お電話番号">
+          <input type="tel" value="{{ old('booking-tel') }}" class="form-control" id="booking-tel" aria-describedby="input_tel" placeholder="お電話番号">
       </div>
       <div class="form-groupe mt-3">
-        <label for="booking-day">ご予約日</label>
-          <input type="date" class="form-control" id="booking-day" aria-describedby="input_day" placeholder="ご予約をご希望の日にち" value="<?php echo date('Y-m-d'); ?>">
+        <label for="booking-date">ご予約日</label>
+          <input type="date" value="{{ old('booking-date') }}" class="form-control" id="booking-date" aria-describedby="input_day" placeholder="ご予約をご希望の日にち" value="<?php echo date('Y-m-d'); ?>">
       </div>
       <div class="form-groupe mt-3">
           <label for="scheduled-time">ご予約時間</label>
-          <input type="time" step="900" class="form-control" id="scheduled-time"> 
+          <input type="time" value="{{ old('booking-time') }}" step="900" class="form-control" id="scheduled-time"> 
       </div>
       <div class="form-groupe mt-3">
           <label for="form-comment">コメント</label>
-          <input type="textarea" class="form-control" id="form-comment" placeholder="伝えたいことなどございましたら、ご記入下さい。" >
+          <input type="textarea" value="{{ old('form-comment') }}" class="form-control" id="form-comment" placeholder="伝えたいことなどございましたら、ご記入下さい。" >
       </div>
       <button class="btn btn-success mt-4"> 入力内容の確認 </button>
     </form>
