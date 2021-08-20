cfae9ff52afad3ca45ed6600a5bf84b196df04fe
rollo <wumabeatboxer@yahoo.co.jp>
2021-08-21 03:55:08 +0900

    I made Withou sys #6

--- resources/views/management/manage.blade.php
+++ resources/views/management/manage.blade.php
@@ -2,10 +2,12 @@
 
 @section('content')
 <div class="container">
-    <h3>{{ $name }}様の予約管理画面</h3><br>
+    <h3>{{ $name }}様のご予約管理画面</h3><br>
     <div>
-    <a class="btn btn-outline-success text-success btn-lg" href="{{ route('edit') }}" role="button" >ご予約の変更はこちら</a><br>
-    <br><a class="btn btn-outline-info text-info btn-lg" href="{{ route('delete') }}" role="button" >ご予約の削除はこちら</a>
+        <label>ご予約の変更→</label>
+            <a class="btn btn-outline-info text-info btn-lg" href="{{ route('edit') }}" role="button" >GO!</a><br>
+        <br><label>ご予約の削除→</label>
+            <a class="btn btn-outline-success text-success btn-lg" href="{{ route('delete') }}" role="button" >GO!</a><br>
     </div>
-</div>
+</div>  
 @endsection
---
+++ resources/views/without/check.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="reservation-form">
+    <form method="POST" action="{{ route('done') }}">
+    @csrf
+      <div class="form-groupe">
+        <label for="booking-name">お名前：</label>
+        {{ $inputs['booking-name'] }}
+        <input name="booking-name" value="{{ $inputs['booking-name'] }}" type="hidden" class="form-control" id="booking-name" aria-describedby="input_name">
+      </div>
+      <div class="form-groupe">
+        <label>お電話番号：</label>
+        {{ $inputs['booking-tel'] }}
+        <input name="booking-tel" value="{{ $inputs['booking-tel'] }}" type="hidden" class="form-control" aria-describedby="input_tel">
+      </div>
+      <div class="form-groupe">
+        <label>ご予約日：</label>
+        {{ $inputs['booking-date'] }}
+        <input name="booking-date" value="{{ $inputs['booking-date'] }}" type="hidden" class="form-control" aria-describedby="input_date">
+      </div>
+      <div class="form-groupe">
+        <label>ご予約時間：</label>
+        {{ $inputs['scheduled-time'] }}
+        <input name="scheduled-time" value="{{ $inputs['scheduled-time'] }}" type="hidden" class="form-control" aria-describedby="scheduled-time">
+      </div>
+    <button type="submit" name="action" class="btn btn-success mt-4" value="submit">予約する</button>
+    <button type="submit" name="action" class="btn btn-info text-white mt-4" value="modify"> 入力内容の変更</button>
+  </form>
+</div>
+@endsection
