6a5ba1f5197318b2105d972e53aff3d942515b4d
rollo <wumabeatboxer@yahoo.co.jp>
2021-09-05 22:13:28 +0900

    Edited config for phpunit

--- tests/Feature/ManageTest.php
+++ tests/Feature/ManageTest.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ManageTest extends TestCase
 {
+
+    use RefreshDatabase;
+    
     /** @test */
     public function only_logged_in_users_can_see_the_reservation_page() 
     {
